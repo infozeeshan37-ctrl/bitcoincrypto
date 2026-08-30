bc61907a5e524ad799a21a866214ce59a3f1c981
infozeeshan37-ctrl <infozeeshan37@gmail.com>
2026-08-30 14:23:57 +0500

    Remove Platform Overview section from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import HomeTradingSuiteHero from "@/components/home/HomeTradingSuiteHero";
 import LiveEcosystemOverview from "@/components/home/LiveEcosystemOverview";
-import PlatformOverview from "@/components/home/PlatformOverview";
 import TrustSecurity from "@/components/home/TrustSecurity";
 import NewsletterCTA from "@/components/home/NewsletterCTA";
 
@@ -13,13 +12,10 @@ export default function HomePage() {
       {/* 2. Real-Time Crypto Ecosystem & Derivatives Intelligence */}
       <LiveEcosystemOverview />
 
-      {/* 3. Platform Architecture & Data Integrity */}
-      <PlatformOverview />
-
-      {/* 4. Institutional Trust & Security Protocol */}
+      {/* 3. Institutional Trust & Security Protocol */}
       <TrustSecurity />
 
-      {/* 5. Intelligence Newsletter Subscription */}
+      {/* 4. Intelligence Newsletter Subscription */}
       <NewsletterCTA />
     </div>
   );
